3c1ca8e667e8eab27f2e0cc5a220dff70b150983
Haley Harms <harms.haley22@gmail.com>
2024-07-01 09:41:05 -0500

    Trying to preform a position cut and get the graph.

--- CRT_Dataset2/Practicecode.C
+++ CRT_Dataset2/Practicecode.C
@@ -6,46 +6,10 @@
 #include "TCanvas.h"
 
 
-void checkRootFile(const std::string& filename) {
-	TFile *file = new TFile(filename.c_str(), "READ");
-	if (file && !file->IsZombie() && file->IsOpen()) {
-		std::cout << "File '" << filename << "' opened successfully." << std::endl;
-	} else {
-		std::cerr << "Failed to open file'" <<filename << "'." << std::endl;
-		if (file) {
-			file->Close();
-			delete file;
-		}
-		exit(EXIT_FAILURE);
-	}
-	if (file) {
-		file->Close();
-		delete file;
-	}
-}
-void navigateToTree(const std::string& filename) {
-	TFile *file = new TFile(filename.c_str(), "READ");
-	TDirectory *crtana_dir = (TDirectory*)file->Get("crtana");
-	if (!crtana_dir) {
-		std::cerr << "Directory 'crtana' not found." << std::endl;
-		file->Close();
-		delete file;
-		exit(EXIT_FAILURE);
-	}
-	TTree *tree = (TTree*)crtana_dir->Get("tree");
-	if (!tree) {
-       		std::cerr << "Tree 'tree' not found within 'crtana'." << std::endl;
-        	file->Close();
-        	delete file;
-        	exit(EXIT_FAILURE);
-	}
-	file->Close();
-	delete file;
-}
-
 void drawVariableFromTree(const std::string& filename, const std::string& variable, const std::string& outputFilename) {
 	TFile *file = new TFile(filename.c_str(), "READ");
-	TTree *tree = (TTree*)file->Get("tree");
+	TDirectory *crtana_dir = (TDirectory*)file->Get("crtana");
+	TTree *tree = (TTree*)crtana_dir->Get("tree");
 	if (!tree) {
 		std::cerr << "Tree 'tree' not found in file '" << filename << "'." << std::endl;
         	file->Close();
@@ -53,6 +17,7 @@ void drawVariableFromTree(const std::string& filename, const std::string& variab
 		return;
 	}
 	TCanvas *canvas1 = new TCanvas("canvas1", "Interactive Cut", 800, 600);
+	std::string drawCommand = variable + " > -500";
 	tree->Draw(variable.c_str());
 	canvas1->Draw();
 	canvas1->SaveAs(outputFilename.c_str());
@@ -63,8 +28,6 @@ void drawVariableFromTree(const std::string& filename, const std::string& variab
 
 int Practicecode() {
 	std::string filename = "/pnfs/sbnd/persistent/users/hlay/crt_comm_summer_2024/run13688_crtana.root";
-	checkRootFile(filename);
-	navigateToTree(filename);
 	std::string variable = "cl_sp_x";
 	std::string outputFilename = "TrialPlot.png";
 	drawVariableFromTree(filename, variable, outputFilename);
