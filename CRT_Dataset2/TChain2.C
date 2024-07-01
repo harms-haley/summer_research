216cdaae65c1aa21aaab7644801abba9b93ad139
Haley Harms <harms.haley22@gmail.com>
2024-07-01 14:40:11 -0500

    has a chain issue im hoping will go away when i restart my computer

--- CRT_Dataset2/TChain2.C
+++ CRT_Dataset2/TChain2.C
@@ -3,6 +3,8 @@
 #include "TChain.h"
 #include "TH1D.h"
 #include "TCanvas.h"
+#include <string>
+
 
 void createAndFillHistogram(const std::string& inputDir, const std::string& outputFilename) {
 	TChain *chain = new TChain("crtana/tree");
@@ -18,7 +20,7 @@ void createAndFillHistogram(const std::string& inputDir, const std::string& outp
 			}
 		}
 	}
-	
+}	
 
 
 
@@ -30,10 +32,12 @@ void createAndFillHistogram(const std::string& inputDir, const std::string& outp
 	std::vector<double> *cl_sp_x = nullptr;
 	std::vector<double> *cl_sp_z = nullptr;
 	std::vector<bool> *cl_has_sp = nullptr;
+	std::vector<double> *cl_sp_ts1 = nullptr;
 
 	chain->SetBranchAddress("cl_has_sp", &cl_has_sp);
 	chain->SetBranchAddress("cl_sp_x", &cl_sp_x);
 	chain->SetBranchAddress("cl_sp_z", &cl_sp_z);
+	chain->SetBranchAddress("cl_sp_ts1", &cl_sp_ts1);
 
 	Long64_t nEntries = chain->GetEntries();
 	Long64_t maxEntriesPerFile = 1000;
@@ -42,17 +46,19 @@ void createAndFillHistogram(const std::string& inputDir, const std::string& outp
 		chain->GetEntry(i);
 		for (size_t k = 0; k < cl_has_sp->size(); ++k) {
 			if ((*cl_has_sp)[k]) {
-				for (size_t j = 0; j < cl_sp_x->size(); ++j) {
-					double x = (*cl_sp_x)[j];
-					double z = (*cl_sp_z)[j];
-					if (-250 < z && z < -150) {
-						histogram->Fill(x);
-						std::cout << "Filled histogram with x = " << x << std::endl; // Print debug message
+				double ts1 = (*cl_sp_ts1)[k];
+				if (ts1 >= 1529733 && ts1 <=1532800) {
+					for (size_t j = 0; j < cl_sp_x->size(); ++j) {
+						double x = (*cl_sp_x)[j];
+						double z = (*cl_sp_z)[j];
+						if (-200 < z && z < -100) {
+							histogram->Fill(x);
+							std::cout << "Filled histogram with x = " << x << std::endl; // Print debug message
+						}	
 					}	
 				}
-			}
-		}	
-	}
+			}		
+		}
 	
 	histogram->GetXaxis()->SetTitle("X");
 	TCanvas *c1 = new TCanvas("c", "1D Histogram", 800, 600);
@@ -66,7 +72,7 @@ void createAndFillHistogram(const std::string& inputDir, const std::string& outp
 
 int TChain2() {
 	std::string inputDir = "/pnfs/sbnd/persistent/users/hlay/crt_comm_summer_2024";
-	std::string outputFilename = "1DHistogram2.png";
+	std::string outputFilename = "1DHistogram3.png";
 	createAndFillHistogram(inputDir, outputFilename);
 	return 0;
 }
